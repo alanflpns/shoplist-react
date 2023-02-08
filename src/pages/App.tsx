387e4caf1cac16230ad37eb9863a7837443925b7
alanflp <alanflpns@gmail.com>
2023-02-08 15:47:58 -0300

    refactor: sconfig icons and pwa

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -38,39 +38,6 @@ function App() {
     }
   }, []);
 
-  useEffect(() => {
-    let deferredPrompt: any;
-    const addBtn = document.getElementById("add-button");
-    if (addBtn) {
-
-      window.addEventListener("beforeinstallprompt", (e) => {
-        console.log('service work on beforeinstallprompt')
-        // Prevent Chrome 67 and earlier from automatically showing the prompt
-        e.preventDefault();
-        // Stash the event so it can be triggered later.
-        deferredPrompt = e;
-        // Update UI to notify the user they can add to home screen
-        addBtn.style.display = "block";
-
-        addBtn.addEventListener("click", (e) => {
-          // hide our user interface that shows our A2HS button
-          addBtn.style.display = "none";
-          // Show the prompt
-          deferredPrompt.prompt();
-          // Wait for the user to respond to the prompt
-          deferredPrompt.userChoice.then((choiceResult: any) => {
-            if (choiceResult.outcome === "accepted") {
-              console.log("User accepted the A2HS prompt");
-            } else {
-              console.log("User dismissed the A2HS prompt");
-            }
-            deferredPrompt = null;
-          });
-        });
-      });
-    }
-  }, []);
-
   function handleCheckProduct(name: string) {
     const productIndex = productList.findIndex(
       (product) => product.name === name
@@ -229,10 +196,6 @@ function App() {
         <strong>Total: </strong>
         <span>R$ {moneyInputFormat(String(totalPrice.toFixed(2)))}</span>
       </div>
-
-      <button className={styles.addButton} id="add-button">
-        Add to home screen
-      </button>
     </main>
   );
 }
